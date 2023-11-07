a8199142e58ef798b4e6315ba956c78b94838274
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-07 18:05:10 -0300

    feat(controller): auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function auth(Request $request) {
+        $user = User::where('email', $request->email)->first();
+        if(! $user && ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        };
+
+        $token = $user->createToken($request->email)->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}
