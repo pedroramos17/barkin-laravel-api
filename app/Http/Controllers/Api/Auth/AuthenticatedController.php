b4a31fb84984fd4d340a591fbc83d7062e23d837
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-08 19:10:30 -0300

    refactor(controller): update Auth name

---
+++ app/Http/Controllers/Api/Auth/AuthenticatedController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /** @throws ValidationException */
+    public function login(AuthRequest $request): JsonResponse {
+        $user = User::where('email', $request->email)->first();
+        if(! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken($request->email)->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
