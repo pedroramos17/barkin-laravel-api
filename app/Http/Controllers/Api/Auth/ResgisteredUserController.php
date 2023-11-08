3f9c8157a35fb57c4fb15c74dee7cf28da54d157
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-08 19:13:37 -0300

    feat(controller): register by token

---
+++ app/Http/Controllers/Api/Auth/ResgisteredUserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+class ResgisteredUserController extends Controller
+{
+    public function store(Request $request): JsonResponse {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        $token = $user->createToken($request->email)->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}
