afea9817730078dacb75f0cc88f6cb7f1e523c05
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-08 19:29:45 -0300

    refactor(routes): finish auth

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\RegisteredUserController;
+use App\Http\Controllers\Api\Auth\AuthenticatedController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/auth', [AuthController::class, 'auth']);
+// Return token
+Route::post('/login', [AuthenticatedController::class, 'login']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
-   Route::post('/logout', [AuthController::class, 'logout']); 
+   Route::post('/logout', [AuthenticatedController::class, 'logout']); 
 });
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
