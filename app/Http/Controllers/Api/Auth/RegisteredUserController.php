0487b52961c2db03701f92dfe267ee781e5d23e1
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-10 01:11:53 -0300

    fix(controller): registered user name

--- app/Http/Controllers/Api/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Api/Auth/RegisteredUserController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-class ResgisteredUserController extends Controller
+class RegisteredUserController extends Controller
 {
     public function store(Request $request): JsonResponse {
         $request->validate([
